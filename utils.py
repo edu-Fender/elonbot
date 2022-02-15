c9f4b7d98f181c9cbca56e1c8dad18f1b1551b88
Eduardo Franco <aneweduardo@hotmail.com>
2022-02-15 13:03:01 -0300

    Changed utils.py as output log file wasn't work properly. Also, changed scheme of the log files

--- utils.py
+++ utils.py
@@ -2,6 +2,16 @@ from datetime import datetime
 
 
 def log(*params):
-    print(datetime.now(), flush=True, *params)
-    with open("output.log", 'a') as f:
-        f.write("{} {}\n".format(datetime.now(), *params))
+
+    now = datetime.now()
+
+    string = ''
+    for i in params:
+        if type(i) is not str:
+            i = str(i)
+        string += i + ' '
+
+    print(now, string, flush=True)
+
+    with open(f"log/output-{now.date()}.log", 'a') as f:
+        f.write("{} {}\n".format(now, string))
